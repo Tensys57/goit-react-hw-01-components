0b1cd14831bc1a332a6d6ec6f9de5e2da3d7a32d
Tensys57 <tensys57@gmail.com>
2023-05-23 00:17:16 +0300

    fixing 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,5 @@
-import { Profile } from './Profile';
-import user from './user.json';
-
-// const App = () => {
-//   return (
-// <>Profile</>
-// <div
-//   style={{
-//     height: '100vh',
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     fontSize: 40,
-//     color: '#010101'
-//   }}
-// >
-//   React homework template
-// </div>
-//   );
-// };
-
-// export default App;
-
-// import UsersList from './Components/UsersList';
-
-// import users from './data/users.json';
-
 const App = () => {
-  return (
-    <>
-      <user user={user} />
-    </>
-  );
+  return <>Profile</>;
 };
 
 export default App;
