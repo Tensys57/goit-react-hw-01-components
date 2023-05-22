133fabcc217f57a4746179eb6627aaa5c88f3970
Tensys57 <tensys57@gmail.com>
2023-05-22 23:29:52 +0300

    config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Profile } from './Profile';
+import user from './user.json';
+
+// const App = () => {
+//   return (
+    <>Profile</>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
+
+export default App;
+
+// import UsersList from './Components/UsersList';
+
+// import users from './data/users.json';
+
+const App = () => {
+  return (
+    <>
+      <UsersList users={user} />
+    </>
+  );
+};
+
+// export default App;
